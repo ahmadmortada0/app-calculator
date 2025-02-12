fb66615cca44dc4821c0e09c3756d93b46ba9639
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-12 20:42:10 +0200

    bulding <> First way

--- ex2.py
+++ ex2.py
@@ -1,3 +1,12 @@
 # 2 ways to solve the equation 
-magic = (input("wanna see a magic trick"))
+magic = input("wanna see a magic trick Y/N :" )
+
+if  magic=="Y":
+    num = int(input("Choose a num :"))
 # first way 
+if num == 0 :
+    print ("even")
+elif num%2==0:
+    print("even")
+else :
+    print("odd")
