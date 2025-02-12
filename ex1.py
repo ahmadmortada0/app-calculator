2c5066e4b96c020e95991e51564f530aa9a04eb1
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-12 20:29:13 +0200

    adding <> message

--- ex1.py
+++ ex1.py
@@ -1,3 +1,9 @@
 age= int(input("Enter Your Age :"))
 if age<=4:
  print("the ticket is unavaible cause your age is "+ str(age))
+elif age<=8:
+ print ("TIcket price is 5$")
+elif age <=15 :
+  print ("TIcket price is 15$")
+else :
+ print ("ticket price is 20$")
