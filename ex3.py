92e6cd5390df1b7f9dec20bd3099a27cb20d4cf8
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-13 19:32:10 +0200

    testing <> ex3

--- ex3.py
+++ ex3.py
@@ -1,2 +1,9 @@
 user_name=input("Enter the user name :")
 password=input(" Enter The password")
+
+user="admin"
+user_password="1234"
+if user_name==user&password==user_password:
+    print("Access granted")
+else :
+    print
