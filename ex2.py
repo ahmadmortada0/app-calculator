eb434171810b12306898454ec18a227dd9f4693e
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-13 19:24:58 +0200

    Finishing <> ex2

--- ex2.py
+++ ex2.py
@@ -5,8 +5,17 @@ if  magic=="Y":
     num = int(input("Choose a num :"))
 # first way 
 if num == 0 :
-    print ("even")
+    print ("the number is even")
 elif num%2==0:
-    print("even")
+    print("the number is even")
 else :
-    print("odd")
+    print("the number is odd")
+#second way
+fnum = float(num)
+power= fnum/2
+if num == 0 :
+    print ("the number is even")
+elif (fnum/2)/int(power)-1==0:
+    print("the number is even")
+else :
+    print("the number is odd")
