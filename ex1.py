710acb798678cfbfb7dcccb8d6d78e78c9f8fcc5
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-13 19:48:12 +0200

    finishing <> ex3

--- ex1.py
+++ ex1.py
@@ -1,9 +1,12 @@
 age= int(input("Enter Your Age :"))
-if age<=4:
- print("the ticket is unavaible cause your age is "+ str(age))
-elif age<=8:
- print ("TIcket price is 5$")
-elif age <=15 :
-  print ("TIcket price is 15$")
-else :
- print ("ticket price is 20$")
+if age <100:
+  if age<=4:
+    print("the ticket is unavaible cause your age is "+ str(age))
+  elif age<=8:
+    print ("TIcket price is 5$")
+  elif age <=15 :
+      print ("TIcket price is 15$")
+  else :
+    print ("ticket price is 20$")
+else:
+  print ("Go rest in your grave")
