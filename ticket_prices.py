2bb9542c93e81dca00bff7b16cff150d605c1fc6
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-14 19:41:28 +0200

    Doing <> clean code

--- ticket_prices.py
+++ ticket_prices.py
@@ -1,7 +1,7 @@
 age= int(input("Enter Your Age :"))
 if age <100:
   if age<=4:
-    print("the ticket is unavaible cause your age is "+ str(age))
+    print("The ticket is unavaible cause your age is "+ str(age))
   elif age<=8:
     print ("TIcket price is 5$")
   elif age <=15 :
